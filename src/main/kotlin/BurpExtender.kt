b2ad879eb11a5c5550292566363fb75cdfa683de
ankio <admin@ankio.net>
2023-10-09 11:34:42 +0800

    init

---
+++ src/main/kotlin/BurpExtender.kt
@@ -0,0 +1,8 @@
+import burp.IBurpExtender
+import burp.IBurpExtenderCallbacks
+
+class BurpExtender : IBurpExtender {
+    override fun registerExtenderCallbacks(callbacks: IBurpExtenderCallbacks) {
+
+    }
+}
